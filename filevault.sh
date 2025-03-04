54045924bbe175b9981b3370063abcddf1ce7828
sack <sachinmehtab@gmail.com>
2025-03-04 15:01:22 +0530

    change

---
+++ filevault.sh
@@ -0,0 +1,1 @@
+echo '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><dict><key>Username</key><string>sachinkumaradmin</string><key>Password</key><string>1111</string></dict></plist>' | sudo fdesetup changerecovery -personal -inputplist
